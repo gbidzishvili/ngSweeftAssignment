48d4fbc9111b25c8fa9ed9caaec1d8bc2dabb7aa
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-04 00:42:54 +0400

    folder sturcture improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeHomePgComponent } from './views/recipe-home-pg/recipe-home-pg.component';
+import { RecipeDetailsPgComponent } from './views/recipe-details-pg/recipe-details-pg.component';
+import { RecipeFormPgComponent } from './views/recipe-form-pg/recipe-form-pg.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: RecipeHomePgComponent,
+  },
+  { path: 'details', component: RecipeDetailsPgComponent },
+  { path: 'form', component: RecipeFormPgComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CoreModule } from './core/core.module';
+import { ViewsModule } from './views/views.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, ViewsModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
