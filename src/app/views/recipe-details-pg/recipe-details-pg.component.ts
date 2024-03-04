8ea88d250c03ac8461e5d19b32a9ae111bee5dc3
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-04 20:00:44 +0400

    last modifications

--- src/app/views/recipe-details-pg/recipe-details-pg.component.ts
+++ src/app/views/recipe-details-pg/recipe-details-pg.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ShareCardDataService } from '../../shared/services/share-card-data.service';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, catchError, of } from 'rxjs';
 import { Recipe } from '../../shared/models/recipe.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,11 +10,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './recipe-details-pg.component.scss',
 })
 export class RecipeDetailsPgComponent implements OnInit {
-  recipeData$!: Observable<Recipe>;
+  recipeData$!: Observable<Recipe | null>;
   constructor(private shareDataService: ShareCardDataService) {}
   ngOnInit(): void {
     this.shareDataService.selectedRecipeId.subscribe((id: any) => {
-      this.recipeData$ = this.shareDataService.getRecipe(id);
+      if (this.shareDataService.getRecipe(id)) {
+        this.recipeData$ = this.shareDataService.getRecipe(id).pipe(
+          catchError((error) => {
+            // Handle the error here
+            console.error('Error fetching recipe:', error);
+            // Return an empty Observable, an Observable with default values, or throwError as needed
+            return of(null); // Example: returning `null` to indicate an error was caught
+          })
+        );
+      }
     });
   }
 }
